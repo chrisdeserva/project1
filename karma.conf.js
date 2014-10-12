0a0568ba51a96828d969570713f43df2041ee041
Chris Deserva <chris.deserva@gmail.com>
2014-10-12 23:48:59 +0800

    Updated karma settings

--- karma.conf.js
+++ karma.conf.js
@@ -10,13 +10,12 @@ module.exports = function(config) {
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['mocha', 'requirejs'],
+    frameworks: ['mocha', 'chai'],
 
 
     // list of files / patterns to load in the browser
     files: [
       {pattern: 'test/*.js', included: false},
-      {pattern: 'test/*', included: false}
     ],
 
 
