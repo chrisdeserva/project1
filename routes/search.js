344d961e26ac39d8b47cbf0ff450d4280bd14a64
Chris Deserva <chris.deserva@gmail.com>
2014-12-02 10:39:54 +0800

    Route with query string

--- routes/search.js
+++ routes/search.js
@@ -1,18 +1,34 @@
 var express = require('express');
 var router = express.Router();
 
-router.param('search', function (req, res, next, search) {
-  console.log('CALLED ONLY ONCE');
-  next();
+router.use(function(req, res, next){
+    //do authentications
+    console.log(req.method, req.url);
+    //get query string
+    console.log("searchItem", req.query.search);
+    next();
 });
 
-router.get('/:search', function(req, res) {
+router.param('search', function (req, res, next, search) {
+    //do validate param
+    console.log('CALLED ONLY ONCE', search);
+    req.search = search+"validated";
+    next();
+});
+
+router.get('/', function(req, res) {
+    console.log("root");
+});
+
+router.get('/item/:search', function(req, res) {
     res.render('search', { 
         home: 'http://localhost:3000', 
-        searchItem: req.params.search,
+        //searchItem: req.params.search,
+        searchItem: req.search,
         result: 'Search result' 
     });
     //res.json({"first":"chris", "last": "deserva"});
 });
 
 module.exports = router;
+
