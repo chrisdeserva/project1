f798d2c3707e8bf89e40a5838349fec90e22667a
Chris Deserva <chris.deserva@gmail.com>
2014-12-26 23:27:46 +0800

    Added listing detail and order dialog

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,21 @@ router.get('/', function(req, res) {
   res.render('index', { home: 'http://localhost:3000' });
 });
 
+router.param('id', function (req, res, next, id) {
+    //do validate param
+    console.log('CALLED ONLY ONCE', id);
+    req.id = id+"validated";
+    next();
+});
+
+router.get('/listing/:id', function(req, res) {
+    res.render('listing', {
+        home: 'http://localhost:3000',
+        //searchItem: req.params.search,
+        product_id: req.id
+    });
+    //res.json({"first":"first", "last": "second"});
+});
+
 module.exports = router;
+
